9611c2a8b5f1d995e04da4c9732af4dc82591961
svinacienitajs <work.gackis@gmail.com>
2024-05-03 21:32:25 +0300

    Beidzot darbs

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    // Define functions for compression
+
+    public static byte[] lz77Compression(byte[] data) {
+        // Implement LZ77 compression algorithm
+        // This function should take uncompressed data as input
+        // and return compressed data using LZ77 algorithm
+    }
+
+    public static byte[] lz77Decompression(byte[] data) {
+        // Implement LZ77 decompression algorithm
+        // This function should take compressed data as input
+        // and return decompressed data using LZ77 algorithm
+    }
+
+    public static byte[] huffmanEncoding(byte[] data) {
+        // Implement Huffman encoding algorithm
+        // This function should take compressed data as input
+        // and return Huffman encoded data
+    }
+    public static byte[] huffmanDecoding(byte[] data) {
+        // Implement Huffman decoding algorithm
+        // This function should take Huffman encoded data as input
+        // and return decompressed data
+    }
+
+    public static byte[] inflate(byte[] data) {
+        // Implement Deflate decompression algorithm
+        // This function should take compressed data as input
+        // and return decompressed data
+    }
+
+    public static byte[] deflate(byte[] data) {
+        // Implement Deflate compression algorithm
+        // This function should take uncompressed data as input
+        // and return compressed data
+    }
+
+    // Main function for testing
+    public static void main(String[] args) {
+        // Test your compression and decompression functions here
+    }
+}
